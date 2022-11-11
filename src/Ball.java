5df81702df3b6bc2e470cce5ac1848bc456a4718
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-11-11 13:30:56 +0100

    Pong + Ball

---
+++ src/Ball.java
@@ -0,0 +1,22 @@
+public class Ball {
+    private int x;
+    private int y;
+    private int vx;
+    private int vy;
+
+    public Ball(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move()
+    {
+
+    }
+
+    public void bounce()
+    {
+
+    }
+}
---
+++ src/Pong.java
@@ -0,0 +1,19 @@
+public class Pong {
+    int width;
+    int height;
+
+    public Pong (int width, int height)
+    {
+
+    }
+
+    public void update()
+    {
+
+    }
+
+    private void checkCollision()
+    {
+
+    }
+}
